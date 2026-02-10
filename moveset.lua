e06a33fd036364965c176da49b5962d91e662273
Squishy <squishy6094@gmail.com>
2026-02-10 15:46:04 -0800

    WIP sounds and movement stuffs

--- moveset.lua
+++ moveset.lua
@@ -1,13 +1,21 @@
 SAMPLE_BOUNCE = audio_sample_load("zbpmsfx-bounce.ogg")
 SAMPLE_TAKE_OFF = audio_sample_load("zbpmsfx-takeoff.ogg")
 
+local PAC_MAN_MAX_SPEED = 40
+
 local gExtrasStates = {}
 for i = 0, MAX_PLAYERS - 1 do
     gExtrasStates[i] = {
         revAudio = audio_stream_load("zbpmsfx-revup.ogg"),
         bounceCount = 0,
+        forceDefaultWalk = false
     }
     audio_stream_set_looping(gExtrasStates[i].revAudio, true)
+end
+
+local function hook_pac_event(hook, func)
+    _G.charSelect.character_hook_moveset(CT_ZBPACMAN, hook, func)
+    _G.charSelect.character_hook_moveset(CT_ZBMSPACMAN, hook, func)
 end
 
 local function lerp_s16(a, b, t)
@@ -23,6 +31,40 @@ local function lerp_s16(a, b, t)
     end
 
     return math.s16(a + delta * t)
+end
+
+local function update_walking_speed(m)
+    if not m then return end
+    local maxTargetSpeed = PAC_MAN_MAX_SPEED
+
+    local slipperyFloor = ((m.area.terrainType & TERRAIN_MASK) == TERRAIN_SNOW and (m.floor ~= nil and m.floor.type & SURFACE_CLASS_SLIPPERY | SURFACE_CLASS_VERY_SLIPPERY ~= 0 ));
+
+    if (m.floor ~= nil) then
+        if (m.floor.type == SURFACE_SLOW) then
+            maxTargetSpeed = PAC_MAN_MAX_SPEED*0.75
+        end
+    end
+
+    local targetSpeed = math.min(m.intendedMag/32*maxTargetSpeed, maxTargetSpeed)
+
+    if (m.quicksandDepth > 10.0) then
+        targetSpeed = targetSpeed * 6.25 / m.quicksandDepth;
+    end
+
+    if (m.forwardVel <= 0.0) then
+        m.forwardVel = m.forwardVel + 1.1;
+    elseif (m.forwardVel <= targetSpeed) then
+        m.forwardVel = m.forwardVel + 1.1 - m.forwardVel / 43.0;
+    elseif (m.floor ~=nil and m.floor.normal.y >= 0.95) then
+        m.forwardVel = m.forwardVel - 1.0;
+    end
+
+    if (m.forwardVel > 48.0) then
+        m.forwardVel = 48.0;
+    end
+
+    m.faceAngle.y = lerp_s16(m.faceAngle.y, m.intendedYaw, 0.3);
+    apply_slope_accel(m);
 end
 
 local function pac_update_air_with_turn(m)
@@ -118,7 +160,9 @@ function mario_detatch_from_floor(m, e, airAction, arg)
     return 0
 end
 
+local ACT_PAC_WALKING = allocate_mario_action(ACT_GROUP_MOVING)
 local ACT_PAC_JUMP = allocate_mario_action(ACT_GROUP_AIRBORNE | ACT_FLAG_AIR | ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION)
+local ACT_PAC_FREEFALL = allocate_mario_action(ACT_GROUP_AIRBORNE | ACT_FLAG_AIR | ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION)
 local ACT_PAC_KICK = allocate_mario_action(ACT_GROUP_AIRBORNE | ACT_FLAG_AIR | ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION)
 local ACT_PAC_REV_CHARGE = allocate_mario_action(ACT_GROUP_STATIONARY)
 local ACT_PAC_REV_ROLL = allocate_mario_action(ACT_GROUP_MOVING)
@@ -128,12 +172,60 @@ local ACT_PAC_BUTT_BOUNCE_LAND = allocate_mario_action(ACT_GROUP_MOVING)
 
 local function pac_gravity(m)
     if not m then return 0 end
-    m.vel.y = math.max(m.vel.y - 4.5, -70)
+    m.vel.y = math.max(m.vel.y - 4.5, -60)
 end
 
 local function pac_butt_bounce_gravity(m)
     if not m then return 0 end
     m.vel.y = m.vel.y - 10
+end
+
+local function act_pac_walking(m)
+    if not m then return 0 end
+    local startPos = {x = 0, y = 0, z = 0}
+    local startYaw = m.faceAngle.y;
+
+    mario_drop_held_object(m);
+
+    if (should_begin_sliding(m) ~= 0) then
+        return set_mario_action(m, ACT_BEGIN_SLIDING, 0);
+    end
+
+    if (m.input & INPUT_FIRST_PERSON ~= 0) then
+        return begin_braking_action(m);
+    end
+
+    if (m.input & INPUT_A_PRESSED ~= 0) then
+        return set_mario_action(m, ACT_JUMP, 0);
+    end
+
+    if (m.controller.buttonDown & B_BUTTON ~= 0) then
+        return set_mario_action(m, ACT_PAC_REV_CHARGE, 0);
+    end
+
+    if (m.input & INPUT_ZERO_MOVEMENT ~= 0) then
+        return begin_braking_action(m);
+    end
+
+    m.actionState = 0;
+
+    vec3f_copy(startPos, m.pos);
+    update_walking_speed(m);
+
+    local step = perform_ground_step(m)
+    if step == GROUND_STEP_LEFT_GROUND then
+        set_mario_action(m, ACT_FREEFALL, 0);
+        set_character_animation(m, CHAR_ANIM_GENERAL_FALL);
+    elseif step == GROUND_STEP_NONE then
+        anim_and_audio_for_walk(m);
+        if (m.intendedMag - m.forwardVel > 16.0) then
+            set_mario_particle_flags(m, PARTICLE_DUST, 0);
+        end
+    end
+
+    check_ledge_climb_down(m);
+    tilt_body_walking(m, startYaw);
+    return 0;
 end
 
 ---@param m MarioState
@@ -169,12 +261,37 @@ local function act_pac_jump(m)
         m.actionTimer = m.actionTimer + 1
     end
 
-    pac_air_action_step(m, ACT_FREEFALL_LAND, anim, 0);
-    if (m.action == ACT_FREEFALL_LAND) then
+    pac_air_action_step(m, ACT_FREEFALL_LAND_STOP, anim, 0);
+    if (m.action == ACT_FREEFALL_LAND_STOP) then
         e.bounceCount = 0
         queue_rumble_data_mario(m, 5, 40);
     end
     --play_flip_sounds(m, 2, 8, 20);
+    return 0;
+end
+
+---@param m MarioState
+local function act_pac_freefall(m)
+    if not m then return 0 end
+    local e = gExtrasStates[m.playerIndex]
+
+    if (m.input & INPUT_B_PRESSED ~= 0) then
+        e.bounceCount = 0
+        return set_mario_action(m, ACT_PAC_KICK, 0);
+    end
+    if m.actionTimer > 5 then
+        if (m.input & INPUT_A_PRESSED ~= 0) then
+            return set_mario_action(m, ACT_PAC_BUTT_BOUNCE, 0);
+        end
+    else
+        m.actionTimer = m.actionTimer + 1
+    end
+
+    pac_air_action_step(m, ACT_FREEFALL_LAND_STOP, CHAR_ANIM_GENERAL_FALL, 0);
+    if (m.action == ACT_FREEFALL_LAND_STOP) then
+        e.bounceCount = 0
+        queue_rumble_data_mario(m, 5, 40);
+    end
     return 0;
 end
 
@@ -187,8 +304,8 @@ local function act_pac_kick(m)
 
     play_mario_sound(m, SOUND_ACTION_TERRAIN_JUMP, CHAR_SOUND_YAHOO);
 
-    pac_air_action_step(m, ACT_FREEFALL_LAND, CHAR_ANIM_AIR_KICK, 0);
-    if (m.action == ACT_FREEFALL_LAND) then
+    pac_air_action_step(m, ACT_FREEFALL_LAND_STOP, CHAR_ANIM_AIR_KICK, 0);
+    if (m.action == ACT_FREEFALL_LAND_STOP) then
         queue_rumble_data_mario(m, 5, 40);
     end
     return 0;
@@ -307,7 +424,7 @@ local function act_pac_rev_roll_air(m)
     end
     
     if (m.input & INPUT_A_PRESSED ~= 0) then
-        return set_mario_action(m, ACT_FREEFALL, 0);
+        return set_mario_action(m, ACT_PAC_FREEFALL, 0);
     end
 
     m.marioObj.header.gfx.animInfo.animAccel = 0x800 * m.forwardVel
@@ -351,7 +468,9 @@ local function act_pac_butt_bounce_land(m)
     return 0;
 end
 
+hook_mario_action(ACT_PAC_WALKING, act_pac_walking)
 hook_mario_action(ACT_PAC_JUMP, {every_frame = act_pac_jump, gravity = pac_gravity})
+hook_mario_action(ACT_PAC_FREEFALL, {every_frame = act_pac_freefall, gravity = pac_gravity})
 hook_mario_action(ACT_PAC_KICK, {every_frame = act_pac_kick, gravity = pac_gravity}, INT_KICK)
 hook_mario_action(ACT_PAC_REV_CHARGE, act_pac_rev_charge)
 hook_mario_action(ACT_PAC_REV_ROLL, act_pac_rev_roll, INT_PUNCH)
@@ -367,6 +486,8 @@ local overrideActs = {
     [ACT_PUNCHING] = ACT_PAC_REV_CHARGE,
     [ACT_MOVE_PUNCHING] = ACT_PAC_REV_CHARGE,
     [ACT_DIVE] = ACT_PAC_REV_CHARGE,
+    [ACT_FREEFALL] = ACT_PAC_FREEFALL,
+    [ACT_WALKING] = ACT_PAC_WALKING,
 
     [ACT_START_CROUCHING] = false,
     [ACT_STOP_CROUCHING] = false,
@@ -379,12 +500,62 @@ local function before_pac_action(m, nextAct)
     audio_stream_pause(e.revAudio)
     if overrideActs[nextAct] then
         if overrideActs[nextAct] ~= false then
-            return set_mario_action(m, overrideActs[nextAct], 0)
+            if nextAct ~= ACT_WALKING or not e.forceDefaultWalk then
+                return set_mario_action(m, overrideActs[nextAct], 0)
+            end
         else
             return 1
         end
     end
 end
 
-_G.charSelect.character_hook_moveset(CT_ZBPACMAN, HOOK_BEFORE_SET_MARIO_ACTION, before_pac_action)
-_G.charSelect.character_hook_moveset(CT_ZBMSPACMAN, HOOK_BEFORE_SET_MARIO_ACTION, before_pac_action)
+
+local forceWalkingInteracts = {
+    [id_bhvDoor] = true,
+    [id_bhvDoorWarp] = true,
+    [id_bhvStarDoor] = true,
+    [id_bhvBowserKeyUnlockDoor] = true,
+    [id_bhvTowerDoor] = true,
+    [id_bhvKoopaShell] = true,
+}
+
+---@param m MarioState
+---@param o Object
+---@param type InteractionType
+local function on_interact(m, o, type)
+    local e = gExtrasStates[m.playerIndex]
+    local bhvID = get_id_from_behavior(o.behavior)
+    if forceWalkingInteracts[bhvID] and m.action == ACT_PAC_WALKING then
+        e.forceDefaultWalk = true
+        set_mario_action(m, ACT_WALKING, 0)
+    end
+
+    if type == INTERACT_COIN then
+
+    end
+end
+
+local wakaSound = audio_sample_load("zbpm-credit.ogg")
+local fruitSound = audio_sample_load("zbpm-fruit.ogg")
+local function on_play_sound(sound, pos)
+    local pos = {
+        x = pos.x - gMarioStates[0].marioObj.header.gfx.cameraToObject.x,
+        y = pos.y - gMarioStates[0].marioObj.header.gfx.cameraToObject.y,
+        z = pos.z - gMarioStates[0].marioObj.header.gfx.cameraToObject.z,
+    }
+    if sound == SOUND_GENERAL_COIN then
+        djui_chat_message_create(tostring(pos.x))
+        djui_chat_message_create(tostring(pos.y))
+        djui_chat_message_create(tostring(pos.z))
+        audio_sample_play(wakaSound, pos, 1)
+        return NO_SOUND
+    end
+    if sound == SOUND_GENERAL_RED_COIN or sound == SOUND_MENU_COLLECT_RED_COIN then
+        audio_sample_play(fruitSound, pos, 1)
+        return NO_SOUND
+    end
+end
+
+hook_pac_event(HOOK_BEFORE_SET_MARIO_ACTION, before_pac_action)
+hook_pac_event(HOOK_ON_INTERACT, on_interact)
+hook_pac_event(HOOK_ON_PLAY_SOUND, on_play_sound)
