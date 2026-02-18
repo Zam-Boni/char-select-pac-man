fe43183c2dd55410c2f729c207f657a65c9ed448
Zam-Boni <sonicboom417@gmail.com>
2026-02-18 11:08:08 -0500

    ramp
    it's tilted up at 11.25 degrees, but you can adjust it in the blend file if you need

---
+++ actors/pacramp/geo.inc.c
@@ -0,0 +1,7 @@
+const GeoLayout pacramp_geo[] = {
+	GEO_NODE_START(),
+	GEO_OPEN_NODE(),
+		GEO_ANIMATED_PART(LAYER_OPAQUE, 0, 0, 0, pacramp_Ramp_Animated_Part_mesh_layer_1),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
